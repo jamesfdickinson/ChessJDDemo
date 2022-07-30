1180c8f00077e51d7c465904dd984026a0575530
Jimmy Dickinson <jamesfdickinson@gmail.com>
2022-07-29 21:01:53 -0700

    files

--- chessOnly.js
+++ chessOnly.js
@@ -3,7 +3,7 @@ import { Chess } from './lib/chess.js'
 //var board = null
 var game = new Chess()
 
-function makeRandomMove () {
+function makeRandomMove() {
   var possibleMoves = game.moves()
 
   // exit if the game is over
@@ -11,11 +11,11 @@ function makeRandomMove () {
 
   var randomIdx = Math.floor(Math.random() * possibleMoves.length)
   game.move(possibleMoves[randomIdx])
- // board.position(game.fen())
+
+  console.log(game.fen());
   console.log(game.ascii());
   window.setTimeout(makeRandomMove, 500)
 }
 
-//board = Chessboard('myBoard', 'start')
 
 window.setTimeout(makeRandomMove, 500)
