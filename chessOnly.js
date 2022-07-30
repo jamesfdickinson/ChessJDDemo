742c13721dabdef102bc24306be300f6e6bbda8a
Jimmy Dickinson <jamesfdickinson@gmail.com>
2022-07-29 20:59:44 -0700

    first commit

---
+++ chessOnly.js
@@ -0,0 +1,21 @@
+import { Chess } from './lib/chess.js'
+
+//var board = null
+var game = new Chess()
+
+function makeRandomMove () {
+  var possibleMoves = game.moves()
+
+  // exit if the game is over
+  if (game.game_over()) return
+
+  var randomIdx = Math.floor(Math.random() * possibleMoves.length)
+  game.move(possibleMoves[randomIdx])
+ // board.position(game.fen())
+  console.log(game.ascii());
+  window.setTimeout(makeRandomMove, 500)
+}
+
+//board = Chessboard('myBoard', 'start')
+
+window.setTimeout(makeRandomMove, 500)
